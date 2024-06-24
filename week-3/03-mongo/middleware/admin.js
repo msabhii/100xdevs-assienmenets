daf35b003f8cd809ceb5b17f330b04be87645673
msabhii <msabhithakur7777@gmail.com>
2024-06-24 19:07:42 +0530

    DB assigenment Done

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,10 @@
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
 }
 
 module.exports = adminMiddleware;
