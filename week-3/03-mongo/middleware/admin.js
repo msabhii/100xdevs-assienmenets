2a92a3a69507484a87cb0c2abcf0d4773c034f0b
msabhii <msabhithakur7777@gmail.com>
2024-06-24 19:30:32 +0530

    Done with Middleware Assignment week 3

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,8 +1,19 @@
+const { Admin } = require("../solution/db");
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
 
+  const res = Admin.findOne({
+    username: username,
+    password: password,
+  });
+  if (res) {
+    return next();
+  } else {
+    res.status(404).send("Sorry");
+  }
   // Implement admin auth logic
   // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
 }
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,18 @@
+const { User } = require("../solution/db");
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  User.findOne({
+    username: username,
+    password: password,
+  }).then(function (value) {
+    if (value) {
+      return next();
+    }else{
+        res.status(404).json({msg:"Not Recoqnisied"})
+    }
+  });
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
 }
 
 module.exports = userMiddleware;
